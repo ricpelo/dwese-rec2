3d6d79074c1f36c7dbc3ef0817e703f849349498
Ricardo Pérez <ricpelo@gmail.com>
2024-04-29 20:11:14 +0200

    Show

--- resources/views/ordenadores/show.blade.php
+++ resources/views/ordenadores/show.blade.php
@@ -1,3 +1,70 @@
 <x-guest-layout>
-    Hola
+    <section class="bg-white dark:bg-gray-900">
+        <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+            <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Ordenador</h2>
+            <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+                <div class="sm:col-span-2">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Marca
+                    </label>
+                    <input type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        value="{{ $ordenador->marca }}" readonly>
+                </div>
+                <div class="w-full">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Modelo
+                    </label>
+                    <input type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        value="{{ $ordenador->modelo }}" readonly>
+                </div>
+                <div class="w-full">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Aula
+                    </label>
+                    <input type="text"
+                        class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                        value="{{ $ordenador->aula->nombre }}" readonly>
+                </div>
+                <div class="sm:col-span-2">
+                    <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                        Histórico de cambios
+                    </label>
+                    <div class="relative overflow-x-auto">
+                        <table class="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                            <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                <tr>
+                                    <th scope="col" class="px-6 py-3">
+                                        Aula origen
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Aula destino
+                                    </th>
+                                    <th scope="col" class="px-6 py-3">
+                                        Instante
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($ordenador->cambios as $cambio)
+                                    <tr class="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                        <td class="px-6 py-4">
+                                            {{ $cambio->origen->nombre }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $cambio->destino->nombre }}
+                                        </td>
+                                        <td class="px-6 py-4">
+                                            {{ $cambio->created_at }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 </x-guest-layout>
